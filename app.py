215c0e6dcf1c5c5e0e902af3f0da880515f4fc42
Thrinesh1200 <63817437+Thrinesh1200@users.noreply.github.com>
2021-02-05 12:22:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,url_for,request
 import pandas as pd 
 import pickle
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer,TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 import pickle
 import nltk
